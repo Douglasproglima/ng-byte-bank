9a786d7381a2503badb496e2d054be39b6acf6fe
Douglas Lima <Douglasproglima@gmail.com>
2022-03-24 23:23:45 -0300

    Decorator output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-byte-bank';
+
+  transfer($event) {
+    console.log($event);
+  };
 }
--- src/app/new-transfer/new-transfer.component.ts
+++ src/app/new-transfer/new-transfer.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-new-transfer',
   templateUrl: './new-transfer.component.html',
   styleUrls: ['./new-transfer.component.scss'],
 })
 export class NewTransferComponent {
+  @Output() toTransfer = new EventEmitter<any>();
+
   value: number = 0;
   destiny: number;
 
   transfer() {
     console.log(`Valor: ${this.value} destino: ${this.destiny}`);
+
+    const emitValue = { value: this.value, destiny: this.destiny };
+    this.toTransfer.emit(emitValue);
   }
 }
